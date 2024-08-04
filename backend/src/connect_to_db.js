e6264e98f8300bf0b2e7e4a1a426c4d8cd9513db
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2024-08-04 11:13:25 +0300

    Feat: Add mongoose connection

---
+++ backend/src/connect_to_db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+// Connect to MongoDB
+export const connectToDB = () => {
+  mongoose.connect("mongodb://localhost:27017/socialize_app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "MongoDB connection error:"));
+  db.once("open", () => {
+    console.log("Connected to MongoDB");
+  });
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,9 @@
 import express from "express";
+import { connectToDB } from "./connect_to_db.js";
 const app = express();
-app.get("/", (req, res)=>res.json({message: "Hello from backend"}))
-app.listen(4000, () => console.log("Socialize running on port 4000!"));
+
+const PORT = process.env.PORT || 3000;
+
+connectToDB();
+app.get("/", (req, res) => res.json({ message: "Hello from backend" }));
+app.listen(4000, () => console.log("Welcome from backend"));
