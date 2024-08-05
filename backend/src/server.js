f457f66bc744c6ebbd895cc5549c65a81e8f4e67
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2024-08-05 07:57:49 +0300

    Chore: add middleware to the server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,18 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import compress from "compression";
+import cors from "cors";
+import helmet from "helmet";
 import { connectToDB } from "./connect_to_db.js";
+import config from "./config/config.js";
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
+app.use(express.json());
+app.use(cookieParser());
+app.use(compress());
+app.use(cors());
+app.use(helmet());
+const PORT = config.port;
 connectToDB();
 app.get("/", (req, res) => res.json({ message: "Hello from backend" }));
-app.listen(4000, () => console.log("Welcome from backend"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
