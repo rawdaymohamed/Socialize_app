69b8fedea95a6770017619e2ea6af0d75532d611
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2024-08-09 03:45:29 +0300

    Feat: Add api calls from frontend to backend for user's crud operations

---
+++ frontend/src/app/utils/api-user.ts
@@ -0,0 +1,73 @@
+export const create = async (user: any) => {
+  try {
+    let response = await fetch("/api/users", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const list = async (signal: any) => {
+  try {
+    let response = await fetch("/api/users/", {
+      method: "GET",
+      signal: signal,
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const read = async (params: any, credentials: any, signal: any) => {
+  try {
+    let response = await fetch("/api/users/" + params.userId, {
+      method: "GET",
+      signal: signal,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const update = async (params: any, credentials: any, user: any) => {
+  try {
+    let response = await fetch("/api/users/" + params.userId, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+      body: JSON.stringify(user),
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+export const remove = async (params: any, credentials: any) => {
+  try {
+    let response = await fetch("/api/users/" + params.userId, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
